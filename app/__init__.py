9ede9a0807713b0aea730e9338547b8de94ce731
Jose Pedro <pedroosd28@gmail.com>
2020-07-09 09:07:32 -0300

    'Booker'

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,8 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-lm = LoginManager(app)
+lm = LoginManager()
+lm.init_app(app)
 
 from app.models import table	
 from app.controllers import default
--- app/models/forms.py
+++ app/models/forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired()])
+	username = StringField('Username', validators=[DataRequired(), Length(min=6, max=25)])
+	password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=25)])
 	remember_me = BooleanField('Remember_me')
 
 class RegisterForm(FlaskForm):
 	email = StringField('email', validators=[DataRequired()])
 	name = StringField('name', validators=[DataRequired()])
-	username = StringField('username', validators=[DataRequired()])
-	password = PasswordField('password', validators=[DataRequired()])
-	terms = BooleanField('terms')
+	username = StringField('username', validators=[DataRequired(), Length(min=6, max=25)])
+	password = PasswordField('password', validators=[DataRequired(), Length(min=8, max=25)])
+	terms = BooleanField('terms', validators=[DataRequired()])
