3849079fb918133483aad0cf7d4573c93e4283ef
Jose Pedro <pedroosd28@gmail.com>
2020-07-10 17:58:35 -0300

    Boocker v01

--- config.py
+++ config.py
@@ -3,8 +3,23 @@ import os.path
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
+SECRET_KEY = 'MinhaNamoradaEUmaGostosinhaLinda'
+
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'storage.db')
-
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY = 'MinhaNamoradaEUmaGostosinhaLinda'
+
+
+'''POSTGRES = {
+    'user': 'postgres',
+    'pw': 'password',
+    'db': 'my_database',
+    'host': 'localhost',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{POSTGRES.user}:\
+{POSTGRES.pw}@{POSTGRES.host}:{POSTGRES.port}/{POSTGRES.db}'''
+
+
+
+
