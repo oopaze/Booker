029f1e386e32d61952cecc9377b5957167540858
Jose Pedro <pedroosd28@gmail.com>
2020-07-11 08:21:55 -0300

    Booker v01

--- config.py
+++ config.py
@@ -5,19 +5,23 @@ DEBUG = True
 
 SECRET_KEY = 'MinhaNamoradaEUmaGostosinhaLinda'
 
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'storage.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-
-'''POSTGRES = {
+POSTGRES = {
     'user': 'postgres',
     'pw': '1112',
     'db': 'flask-app',
     'host': 'localhost',
     'port': '5432',
 }
+<<<<<<< HEAD
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{POSTGRES.user}:{POSTGRES.pw}@{POSTGRES.host}:{POSTGRES.port}/{POSTGRES.db}'''
+=======
+
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'storage.db')
+SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES['user']}:{POSTGRES['pw']}@{POSTGRES['host']}:{POSTGRES['port']}/{POSTGRES['db']}"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+>>>>>>> 31550b4e8008fca927ab9d5ce294d820d4aaec71
 
 
 
