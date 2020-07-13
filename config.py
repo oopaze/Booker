854a66c8d4ca8ccc7fb730bca66fdbc6aa5942bf
Jose Pedro <pedroosd28@gmail.com>
2020-07-13 06:55:45 -0300

    add classes on config.py

--- config.py
+++ config.py
@@ -8,24 +8,18 @@ class Config(object):
 	TESTING = False
 
 class ProductionConfig(Config):
-	PSQL = {
-		'user' : 'zozdgdwbfdgnwu',
-		'pw' : 'b146f7bfd1bf7069dc41901382b97ca8b7a49bbc038ed4414a444a837afb7ca3',
-		'endereco' : 'ec2-34-236-215-156.compute-1.amazonaws.com',
-		'db' : 'd8cqcaclcmulpo',
-		'port': '5432'
-		}
+	"""
+	postgres = 'postgres://zozdgdwbfdgnwu:b146f7bfd1bf7069dc41901382b97ca8b7a49bbc038ed4414a444a837afb7ca3@ec2-34-236-215-156.compute-1.amazonaws.com:5432/d8cqcaclcmulpo'
 	
-	postgres = f'postgres://{PSQL["user"]}:PSQL["pw"]@{PSQL["endereco"]}:{PSQL["port"]}/{PSQL["db"]}'
-	DATABASE_URI = postgres
+	DATABASE_URI = postgres"""
 	SQLALCHEMY_DATABASE_URI = DATABASE_URI
 	DEBUG = False
 
 class DevelopmentConfig(Config):
 	basedir = os.path.abspath(os.path.dirname(__file__))
 	sqlite = 'sqlite:///' + os.path.join(basedir, 'storage.db')
-	DATABASE_URI = sqlite
-	SQLALCHEMY_DATABASE_URI = DATABASE_URI
+	
+	SQLALCHEMY_DATABASE_URI = sqlite
 	DEBUG = True
 
 class TestingConfig(Config):
