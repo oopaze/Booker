0edd65b629161b07479cbdd631b9f2b10b3d2385
Jose Pedro <pedroosd28@gmail.com>
2020-07-15 21:05:04 -0300

    Fixing BD

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import click
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_login import LoginManager
+from flask.cli import with_appcontext
 
 app =  Flask(__name__)
 app.config.from_object('config.ProductionConfig')
@@ -17,5 +19,16 @@ manager.add_command('db', MigrateCommand)
 lm = LoginManager()
 lm.init_app(app)
 
+@click.command(name='create')
+@with_appcontext
+def create():
+	db.drop_all()
+	db.create_all()
+
+app.cli.add_command(create)
+
 from app.models import table	
 from app.controllers import default
+
+
+
