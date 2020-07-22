46308bdfa769a7a919c5b99f689236e498d95005
Jose Pedro <pedroosd28@gmail.com>
2020-07-21 22:22:51 -0300

    Fixing Postgres BD

--- config.py
+++ config.py
@@ -5,10 +5,6 @@ from os import environ
 class Config(object):
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SECRET_KEY = 'MinhaNamoradaEUmaGostosinhaLinda'
-
-	basedir = os.path.abspath(os.path.dirname(__file__))
-	sqlite = 'sqlite:///' + os.path.join(basedir, 'storage.db')
-	DATABASE_URI = sqlite
 	
 	DEBUG = False
 	TESTING = False
@@ -17,7 +13,7 @@ class ProductionConfig(Config):
 	postgres = 'postgres://gyiwsztabkirow:9caef036b7e4a88e8f8d8f2ab29f0a13302fcd17c27ad4fc6983c521ca90fd01@ec2-34-233-226-84.compute-1.amazonaws.com:5432/df1anrfho1a3ce'
 	
 	DATABASE_URI = postgres
-	SQLALCHEMY_DATABASE_URI = environ['DATABASE_URI']
+	SQLALCHEMY_DATABASE_URI = DATABASE_URI
 	DEBUG = False
 
 class DevelopmentConfig(Config):
